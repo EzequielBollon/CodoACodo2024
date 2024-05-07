d822bffd70cdbc84a975ec705b6115d0ec3e9d34
bautiCap <bautidragon@hotmail.com>
2024-05-07 15:43:13 -0300

    HeaderFooterHTML----->JS

---
+++ assets/Js/footer.js
@@ -0,0 +1,26 @@
+if(document.querySelector(".footer-index")){
+    document.querySelector(".footer-index").innerHTML=`
+    <div>
+        <p>&copy; 2024 HardSoft. Todos los derechos reservados.</p>
+    </div>
+        <ul class="social-icons">
+            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa-solid fa-x"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+        </ul> 
+    `
+}
+else{
+    document.querySelector(".footer-pages").innerHTML=`
+    <div>
+        <p>&copy; 2024 HardSoft. Todos los derechos reservados.</p>
+    </div>
+        <ul class="social-icons">
+            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa-solid fa-x"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+        </ul> 
+    `
+}
