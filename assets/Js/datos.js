fae7a1c8fab7c6d7795d64c82f54e86105044df8
bautiCap <bautidragon@hotmail.com>
2024-05-10 14:48:27 -0300

    agrege js con datos de los productos, repare el link js de contacto,cambie nombre de imagenes

---
+++ assets/Js/datos.js
@@ -0,0 +1,57 @@
+var data= [
+    {
+    "id": 1,
+    "title":"Tablet",
+    "description":"tablet 8gb ram, Samsung",
+    "image":"assets/images/tarjetas/prod${i}.png",
+    "price": "200.000"
+    },
+    {
+    "id": 2,
+    "title":"Pc escritorio",
+    "description":" cpu 123, ram 4gb, gtx 1090",
+    "image":"assets/images/tarjetas/prod${i}.png",
+    "price":"500.000" 
+    },
+    {
+    "id": 3,
+    "title":"Mouse",
+    "description":" mouse razer scorpion",
+    "image":"assets/images/tarjetas/prod[i].png",
+    "price": "20.000"
+    },
+    {
+    "id": 4,
+    "title":"Notebook",
+    "description":" lenovo 8gb ram, Cpu celeron",
+    "image":"../images/tarjetas/prod.png",
+    "price": "450.000"
+    },
+    {
+    "id": 5,
+    "title":"Notebook",
+    "description":" Asus 12gb Ram, tx5600",
+    "image":"../images/tarjetas/prod.png",
+    "price": "600.000"
+    },
+    {
+    "id": 6,
+    "title":"Teclado",
+    "description":" teclado reddragon, mecanico",
+    "image":"../images/tarjetas/prod.png",
+    "price": "26.000"
+    },
+    {
+    "id": 7,
+    "title":"Auriculares",
+    "description":" Razer lite HQ",
+    "image":"../images/tarjetas/prod.png",
+    "price": "20.000"
+    },
+    {
+    "id": 8,
+    "title":"Gabinete PC",
+    "description":"gabiente con 3coolers, vidrio templado",
+    "image":"../images/tarjetas/prod.png",
+    "price": "60.000"
+    }]
--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,17 +1,19 @@
 // ACCEDER AL HTML POR MEDIO DEL DOM PARA AGREGAR TARJETAS DE PRODUCTOS DINAMICAMENTE EN EL CONTENEDOR.
-let contenedor = document.querySelector('.contenedor');
+
+let contenedor = document.querySelector(".contenedor");
 let i=0;
-while(i++<=7){
+while(i<=data.length){
     let card = ` <div data-aos="zoom-in" data-aos-duration="700">
-                    <div class="card card1">
-                        <img src="assets/images/tarjetas/prod${i}.png" alt="Imagen ${i}" class="card-img">
+                    <div class="card card${i}">
+                         <img src="assets/images/tarjetas/prod${i}.png" alt="producto ${i}" class="card-img">
                         <div class="card-content">
-                            <h3 class="card-title">Producto ${i}</h3>
-                            <p class="card-description">Descripción breve del producto.</p>
-                            <p class="card-price">$50</p>
+                            <h3 class="card-title">${data[i].title}</h3>
+                            <p class="card-description">${data[i].description}</p>
+                             <p class="card-price">${data[i].price}$</p>
                             <a href="#" class="card-btn">Ver más</a>
                         </div>
                     </div>
                 </div>`;
     contenedor.innerHTML+=card;
+    i++;
 }
