415309eefc6ddc214a0374f724c1e868476064fa
EzequielBollon <ezequiel_bollon@hotmail.com>
2024-05-21 11:03:20 -0300

    Se realizo cambios en productos para la entrega, se aguarda menu hamburguesa

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -5,7 +5,7 @@ let i=0;
 while(i<=data.length){
     let card = ` <div data-aos="zoom-in" data-aos-duration="700">
                     <div class="card card${i}">
-                         <img src="assets/images/tarjetas/prod${i}.png" alt="producto ${i}" class="card-img">
+                         <img src="../assets/images/tarjetas/prod${i}.png" alt="producto ${i}" class="card-img">
                         <div class="card-content">
                             <h3 class="card-title">${data[i].title}</h3>
                             <p class="card-description">${data[i].description}</p>
