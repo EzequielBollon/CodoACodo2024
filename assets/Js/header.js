e942c46f7fa9f326df4437dadd1858eed43f25bd
Santiago <santiagotorradouba@gmail.com>
2024-05-22 10:55:27 -0300

    menu hamburguesa funcional, falta ocultar indice y agregar barra de busqueda a la barra de menu

--- assets/Js/header.js
+++ assets/Js/header.js
@@ -1,71 +1,57 @@
-// Para ahorrar código, ahora solo hay que modificar el js para que se modifiquen todos los header
-
-if (document.querySelector(".headerIndex")) {
-    document.querySelector(".headerIndex").innerHTML = `
+document.addEventListener('DOMContentLoaded', function() {
+    // Determinar si estamos en la página de inicio o en otra página
+    if (document.querySelector(".headerIndex")) {
+        document.querySelector(".headerIndex").innerHTML = `
+        <nav class="navbar">
         <div class="logo">
-            <a href="index.html"><img src="assets/images/logo.PNG" alt=""></a>
+            <a href="index.html"><img src="assets/images/logo.PNG" alt="HardSoft Logo"></a>
             <a href="index.html"><h1>HardSoft</h1></a>
         </div>
-        <!-- Menu Hamburguesa -->
-        <div class="menuHamburguesa">
-            <input type="checkbox">
-            <i class="fas fa-bars"></i>
-            <i class="fas fa-times"></i>
-            <nav>
-                <ul>
-                    <li><a href="ham">Item 1</a></li>
-                    <li><a href="ham">Item 2</a></li>
-                    <li><a href="ham">Item 3</a></li>
-                    <li><a href="ham">Item 4</a></li>
-                    <li><a href="ham">Item 5</a></li>
+        <label class="labe_hamburguesa" for="menu_hamburguesa">
+            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="list_icon" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+        </label>
+        <input class="menu_hamburguesa" type="checkbox" id="menu_hamburguesa">
+        <ul class="ul_links">
+            <li class="li_links"><a href="index.html" class="link">Inicio</a></li>
+            <li class="li_links"><a href="pages/producto.html" class="link">Productos</a></li>
+            <li class="li_links"><a href="pages/login.html" class="link">Inicia sesión</a></li>
+            <li class="li_links"><a href="pages/contacto.html" class="link">Contacto</a></li>
+        </ul>
+    </nav>
+    <form class="barra-busqueda" action="#">
+        <span class="material-symbols-outlined">search</span>
+        <input type="text" placeholder="Buscar Producto...">
+        <button type="submit" class="btn-buscar">Buscar</button>
+    </form>
+    `;
+    } else {
+        document.querySelector(".headerPages").innerHTML = `
+        <nav class="navbar">
+                <div class="logo">
+                    <a href="../index.html"><img src="../assets/images/logo.PNG" alt="HardSoft Logo"></a>
+                    <a href="../index.html"><h1>HardSoft</h1></a>
+                </div>
+                <label class="labe_hamburguesa" for="menu_hamburguesa">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="list_icon" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10 a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                </label>
+                <input class="menu_hamburguesa" type="checkbox" id="menu_hamburguesa">
+                <ul class="ul_links">
+                    <li class="li_links"><a href="../index.html" class="link">Inicio</a></li>
+                    <li class="li_links"><a href="producto.html" class="link">Productos</a></li>
+                    <li class="li_links"><a href="login.html" class="link">Inicia sesión</a></li>
+                    <li class="li_links"><a href="contacto.html" class="link">Contacto</a></li>
                 </ul>
             </nav>
-        </div>
-        <form class="barra-busqueda" action="#">
-            <span class="material-symbols-outlined">search</span>
-            <input type="text" placeholder="Buscar Producto...">
-        </form>
-        <nav>
-            <ul>
-                <li><a href="index.html">Inicio</a></li>
-                <li><a href="pages/producto.html">productos</a></li>
-                <li><a href="pages/login.html">Inicia sesión</a></li>
-                <li><a href="pages/contacto.html">Contacto</a></li>
-            </ul>
-        </nav>`;
-} else {
-    document.querySelector(".headerPages").innerHTML = `
-        <div class="logo">
-            <a href="../index.html"><img src="../assets/images/logo.PNG" alt=""></a>
-            <a href="../index.html"><h1>HardSoft</h1></a>
-        </div>
-        <!-- Menu Hamburguesa -->
-        <div class="menuHamburguesa">
-            <input type="checkbox">
-            <i class="fas fa-bars"></i>
-            <i class="fas fa-times"></i>
-            <nav>
-                <ul>
-                    <li><a href="ham">Item 1</a></li>
-                    <li><a href="ham">Item 2</a></li>
-                    <li><a href="ham">Item 3</a></li>
-                    <li><a href="ham">Item 4</a></li>
-                    <li><a href="ham">Item 5</a></li>
-                </ul>
-            </nav>
-        </div>
-        <form class="barra-busqueda" action="#">
-            <span class="material-symbols-outlined">search</span>
-            <input type="text" placeholder="Buscar Producto...">
-        </form>
-        <nav>
-            <ul>
-                <li><a href="../index.html">Inicio</a></li>
-                <li><a href="producto.html">productos</a></li>
-                <li><a href="login.html">Inicia sesión</a></li>
-                <li><a href="contacto.html">Contacto</a></li>
-            </ul>
-        </nav>`;
-}
+            <form class="barra-busqueda" action="#">
+                <span class="material-symbols-outlined">search</span>
+                <input type="text" placeholder="Buscar Producto...">
+                <button type="submit" class="btn-buscar">Buscar</button>
+            </form>
+        `;
+    }
 
-
+});
---
+++ assets/Js/header1.js
@@ -0,0 +1,67 @@
+// // Para ahorrar código, ahora solo hay que modificar el js para que se modifiquen todos los header
+
+// if (document.querySelector(".headerIndex")) {
+//     document.querySelector(".headerIndex").innerHTML = `
+//         <div class="logo">
+//             <a href="index.html"><img src="assets/images/logo.PNG" alt=""></a>
+//             <a href="index.html"><h1>HardSoft</h1></a>
+//         </div>
+//         <!-- Menu Hamburguesa -->
+//         <div class="menuHamburguesa">
+//             <input type="checkbox">
+//             <i class="fas fa-bars"></i>
+//             <i class="fas fa-times"></i>
+//             <nav>
+//                 <ul>
+//                 <li><a href="index.html">Inicio</a></li>
+//                 <li><a href="pages/producto.html">productos</a></li>
+//                 <li><a href="pages/login.html">Inicia sesión</a></li>
+//                 <li><a href="pages/contacto.html">Contacto</a></li>
+//             </ul>
+//             </nav>
+//         </div>
+//         <form class="barra-busqueda" action="#">
+//             <span class="material-symbols-outlined">search</span>
+//             <input type="text" placeholder="Buscar Producto...">
+//         </form>
+//         <nav>
+//             <ul>
+//                 <li><a href="index.html">Inicio</a></li>
+//                 <li><a href="pages/producto.html">productos</a></li>
+//                 <li><a href="pages/login.html">Inicia sesión</a></li>
+//                 <li><a href="pages/contacto.html">Contacto</a></li>
+//             </ul>
+//         </nav>`;
+// } else {
+//     document.querySelector(".headerPages").innerHTML = `
+//         <div class="logo">
+//             <a href="../index.html"><img src="../assets/images/logo.PNG" alt=""></a>
+//             <a href="../index.html"><h1>HardSoft</h1></a>
+//         </div>
+//         <!-- Menu Hamburguesa -->
+//         <div class="menuHamburguesa">
+//             <input type="checkbox">
+//             <i class="fas fa-bars"></i>
+//             <i class="fas fa-times"></i>
+//             <nav>
+//                 <ul>
+//                 <li><a href="../index.html">Inicio</a></li>
+//                 <li><a href="producto.html">productos</a></li>
+//                 <li><a href="login.html">Inicia sesión</a></li>
+//                 <li><a href="contacto.html">Contacto</a></li>
+//             </ul>
+//             </nav>
+//         </div>
+//         <form class="barra-busqueda" action="#">
+//             <span class="material-symbols-outlined">search</span>
+//             <input type="text" placeholder="Buscar Producto...">
+//         </form>
+//         <nav>
+//             <ul>
+//                 <li><a href="../index.html">Inicio</a></li>
+//                 <li><a href="producto.html">productos</a></li>
+//                 <li><a href="login.html">Inicia sesión</a></li>
+//                 <li><a href="contacto.html">Contacto</a></li>
+//             </ul>
+//         </nav>`;
+// }
